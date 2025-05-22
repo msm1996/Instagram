cc8ce704b29dda19eb09fa6e3f789f1d7f69b3e4
msm1996 <master19041996@gmail.com>
2025-05-21 22:29:49 -0300

    Site do Instagram usando HTML, CSS e JavaScript.

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelector(".texto-1").onclick = function () {
+    window.open("https://www.facebook.com/index.php?next=https%3A%2F%2Fwww.facebook.com%2Foidc%2F%3Fapp_id%3D124024574287414%26redirect_uri%3Dhttps%253A%252F%252Fwww.instagram.com%252Faccounts%252Fsignupviafb%252F%26response_type%3Dcode%26scope%3Dopenid%2Bemail%2Bprofile%2Blinking%26state%3DATmaUkojcLtUaEB1GZRbkkqmhBzlnG0ynt744qxDPFXxzX7SUPrUOwVBrgqlMD8bKZqDukXiOQ9Uhg998x78rfHj89MkehcshoJ-hHX7Z1VEXGINVlDIw4kaajA0dEG7TM4zfwkaZEu6PGnW3dRB5yHBhRL_oLd3hUlgQu_fMC38i4ycCBQj9vIumV3S6-m0yDQrt7gVTfyHthPoze30j_A-e3Uv6UljQDNf1UIhqMCLu0HSskYGxJ54w7HtuBJBqQBB&deoia=1&no_universal_links=1", "_blank");
+}
+
+document.querySelector(".link").onclick = function () {
+    window.open("https://www.instagram.com/accounts/password/reset/?next=%2Faccounts%2Fpassword%2Freset%2F", "_blank");
+}
+
+document.querySelector(".texto-2").onclick = function () {
+    window.open("https://www.instagram.com/accounts/emailsignup/", "_blank");
+}
